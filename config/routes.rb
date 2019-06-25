ab6395e3b899da7da0c695493e9e261011a0c464
Kendra <klockard@oberlin.edu>
2019-06-24 17:21:20 -0700

    Update home and header layout for logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,19 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'static_pages#home'
+  get '/home',     to: 'static_pages#my_home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+
   get '/signup',    to: 'users#new'
   post '/signup',  to: 'users#create'
+
   get '/login',    to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   patch '/users/:id/edit', to: 'users#edit'
+
   resources :users
 end
