9264e44c041ad237b2b9f4662c3c62f0714ac32d
Kendra <klockard@oberlin.edu>
2019-07-11 12:19:44 -0700

    Update assets pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,8 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile << Proc.new { |path|
-  if path =~ /\.(eot|svg|ttf|woff)\z/
-    true
-  end
-}
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
