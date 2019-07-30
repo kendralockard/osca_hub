2236ce2fe65d3ba53278039a1162d819175c77dc
Kendra <klockard@oberlin.edu>
2019-07-30 16:50:37 -0700

    Require @oberlin.edu email accounts for users
    Fixes #22

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,10 @@ class User < ApplicationRecord
   before_create :create_activation_digest
 
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@oberlin\.edu+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
+                    format: { with: VALID_EMAIL_REGEX,
+                              message: ": Please sign up with your Oberlin email." },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
