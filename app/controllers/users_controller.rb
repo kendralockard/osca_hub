9be704dd249b70871fc65303ae7cc0da11fb0a03
Kendra <klockard@oberlin.edu>
2019-07-31 16:42:55 -0700

    Pull javascript out of views
    Fixes #64

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,10 +33,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    prev_coop = @user.coop_id
+    coop = @user.coop_id
     if @user.update_attributes(user_params)
-      switched_coop?(prev_coop) ?
-        request_switch(prev_coop) :
+      user_requested_switch?(coop) ?
+        request_switch(coop) :
         flash[:success] = "Settings updated"
       redirect_to @user
     else
@@ -44,27 +44,25 @@ class UsersController < ApplicationController
     end
   end
 
-  def coops
-    ["", "Brown Bag", "Fairkid", "Harkness", "Keep", "Pyle", "Tank", "TWC"]
+  def user_requested_switch?(coop)
+    @user.coop_id != coop
   end
 
-  def switched_coop?(prev_coop)
-    @user.coop_id != prev_coop
-  end
+  def request_switch(coop)
+    @requested = @user.coop_id            # Grab coop user requested to join
+    @user.update_attributes(coop_id: coop)     # Put user back in their original coop
 
-  def request_switch(prev_coop)
-    @requested_coop = @user.coop_id
-    @user.update_attributes(coop_id: prev_coop)
-    UserMailer.request_switch(@user, @requested_coop).deliver_now
-    flash[:success] = "Your request to join " + coops()[@requested_coop] + \
-                       " has been sent to your MemCo."
+    UserMailer.request_switch(@user, @requested).deliver_now
+    flash[:success] = "Your request to join #{Coop.coops[@requested]} \
+                       has been sent to your MemCo."
   end
 
   def approve_user
     user = User.find(params[:id])
-    user.update_attributes(coop_id: params[:coop_id])
-    flash[:success] = user.name + " has been admitted to " + \
-                      coops()[user.coop_id] + ". Thanks MemCo!"
+    user.update_attributes(coop_id: params[:coop_id])   # Put user in their requested coop
+    flash[:success] = "#{user.name} has been admitted to #{Coop.coops[user.coop_id]}. \
+                       Thanks MemCo!"
+
     redirect_to root_url
     notify_user_approval(user, params[:coop_id])
   end
