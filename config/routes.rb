f375c2b5af061e8bcaca24440afc52078ff9354c
Kendra <klockard@oberlin.edu>
2019-08-02 16:05:27 -0700

    Allow sub requests to be pushed through email
    Fixes #70

--- config/routes.rb
+++ config/routes.rb
@@ -31,13 +31,14 @@ Rails.application.routes.draw do
   get '/join_coop', to: "coops#join"
   patch '/join_coop', to: "coops#join"
 
-  post '/announcements/:id', to: "announcements#notify_thru_email"
+  post '/announcements/:id', to: "announcements#push_announcement"
+  post '/events/:id', to: "events#push_event"
 
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :announcements,       only: [:create, :destroy, :notify_thru_email]
+  resources :announcements,       only: [:create, :destroy, :push_announcement]
   resources :menus,               only: [:create, :destroy]
-  resources :events,              only: [:create, :destroy]
+  resources :events,              only: [:create, :destroy, :push_event]
   resources :coops
 end
