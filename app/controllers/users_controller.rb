415517613f5010977e36fa2ad57dce9879783a94
Kendra <klockard@oberlin.edu>
2019-08-02 16:30:16 -0700

    Remove Users page
    Fixes #69

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,6 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
-  def index
-    @users = User.where(activated: true).paginate(page: params[:page])
-  end
-
   def show
     @user = User.find(params[:id])
     @announcements = @user.announcements.paginate(page: params[:page])
