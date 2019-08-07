eafbdd1e24c505d6786bee823e372ed2230f18d7
Kendra <klockard@oberlin.edu>
2019-08-07 09:15:26 -0700

    Clean/precompile assets

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,6 +1,7 @@
 function showComments(id) {
    document.getElementById("reveal-"+id).style.display = "none";
    document.getElementById("hide-"+id).style.display = "block";
+   document.getElementById("comments-"+id).style.padding = "0rem";
    $("#comments-"+id).fadeIn(300);
 }
 
@@ -10,16 +11,25 @@ function hideComments(id) {
    document.getElementById("comments-"+id).style.display = "none";
 }
 
-function postComment(id) {
-   document.getElementById("reveal-"+id).style.display = "none";
+function postComment(id, comments) {
+  if (comments) {
+    document.getElementById("reveal-"+id).style.display = "none";
+    if (document.getElementById("hide-"+id).style.display == "block") {
+      document.getElementById("hide-"+id).style.display = "none";
+      document.getElementById("comments-"+id).style.padding = "2rem";
+    }
+  }
    document.getElementById("post-comment-"+id).style.display = "block";
    document.getElementById("post__comment-"+id).style.display = "none";
    document.getElementById("cancel__comment-"+id).style.display = "inline-block";
 }
 
-function cancelComment(id) {
+function cancelComment(id, comments) {
   document.getElementById("cancel__comment-"+id).style.display = "none";
   document.getElementById("post-comment-"+id).style.display = "none";
   document.getElementById("post__comment-"+id).style.display = "inline-block";
-  document.getElementById("reveal-"+id).style.display = "block";
+  if (comments) {
+    document.getElementById("reveal-"+id).style.display = "block";
+  }
+  hideComments(id);
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
-  config.public_file_server.enabled = true
-  config.assets.enabled = true
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -31,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
