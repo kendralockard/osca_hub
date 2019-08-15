1b2b0606e645a7144f02b57537dde9e8e98e17aa
Kendra <klockard@oberlin.edu>
2019-08-15 15:48:46 -0700

    Allow users to directly change profile pictures
    Fixes #111

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,14 @@
 module UsersHelper
 
-  # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { size: 80 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=identicon"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  # Returns the avatar for the given user.
+  def avatar_for(user, options = { size: 80 })
+    if user.avatar.attached?
+      image_tag(url_for(user.avatar), alt: user.name, class: "avatar", width: options[:size])
+    else
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      size = options[:size]
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=identicon"
+      image_tag(gravatar_url, alt: user.name, class: "avatar")
+    end
   end
 end
