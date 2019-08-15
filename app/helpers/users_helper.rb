0fa405f5fcf436d7c6f0353c4096d0302c6163c3
Kendra <klockard@oberlin.edu>
2019-08-15 16:34:05 -0700

    Resize user avatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,13 @@
 module UsersHelper
 
   # Returns the avatar for the given user.
-  def avatar_for(user, options = { size: 80 })
+  def avatar_for(user, options = { size: 50 })
+    size = options[:size]
     if user.avatar.attached?
-      image_tag user.avatar.variant(resize: "80x80>")
+      image_tag(url_for(user.avatar), alt: user.name, class: "avatar", width: size)
     else
       gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-      size = options[:size]
+      class_name = options[:class_name]
       gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=identicon"
       image_tag(gravatar_url, alt: user.name, class: "avatar")
     end
